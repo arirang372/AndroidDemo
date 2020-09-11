a7319ec6f94a1c0f42b584bd90daf22ac01d0aa3
arirang372 <hyonoo419@gmail.com>
2020-09-11 11:51:39 -0400

    done

---
+++ AceTelematicsModel.java
@@ -0,0 +1,72 @@
+package com.geico.mobile.android.ace.geicoapppresentation.telematics.mvvm.model;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.ObservableArrayList;
+import androidx.databinding.ObservableBoolean;
+import androidx.databinding.ObservableList;
+
+import com.geico.mobile.android.ace.geicoappmodel.AceViewBackingModel;
+import com.geico.mobile.android.ace.mitsupport.mitmodel.telematics.AceTelematicsDriver;
+import com.geico.mobile.android.ace.mitsupport.mitmodel.telematics.AceTelematicsRelatedDriver;
+
+import java.util.List;
+
+/**
+ * Models data needed to display on {@link com.geico.mobile.android.ace.geicoapppresentation.telematics.mvvm.view.AceTelematicsActivity}
+ * and to use as part of communication with MCIT and vendor SDKs.
+ *
+ * @author Austin Morgan
+ */
+public class AceTelematicsModel extends AceViewBackingModel {
+
+    private final AceTelematicsDashboardModel dashboardModel = new AceTelematicsDashboardModel();
+    private final ObservableList<AceTelematicsRelatedDriver> relatedDrivers = new ObservableArrayList<>();
+    private final AceTelematicsScoreDetailsModel scoreDetailsModel = new AceTelematicsScoreDetailsModel();
+    private final AceTelematicsTripDetailsModel tripDetailsModel = new AceTelematicsTripDetailsModel();
+    private final ObservableBoolean showLogInForMoreOptions = new ObservableBoolean(false);
+    private AceTelematicsDriver driver = new AceTelematicsDriver();
+
+    public void setShouldShowLogInForMoreOptions(boolean showLogInForMoreOptions) {
+        this.showLogInForMoreOptions.set(showLogInForMoreOptions);
+    }
+
+    @NonNull
+    public ObservableBoolean shouldShowLogInForMoreOptions() {
+        return showLogInForMoreOptions;
+    }
+
+    @NonNull
+    public AceTelematicsDashboardModel getDashboardModel() {
+        return dashboardModel;
+    }
+
+    @NonNull
+    public AceTelematicsDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(@NonNull AceTelematicsDriver driver) {
+        this.driver = driver;
+    }
+
+    @NonNull
+    public ObservableList<AceTelematicsRelatedDriver> getRelatedDrivers() {
+        return relatedDrivers;
+    }
+
+    public void setRelatedDrivers(@NonNull List<AceTelematicsRelatedDriver> relatedDrivers) {
+        this.relatedDrivers.clear();
+        this.relatedDrivers.addAll(relatedDrivers);
+    }
+
+    @NonNull
+    public AceTelematicsScoreDetailsModel getScoreDetailsModel() {
+        return scoreDetailsModel;
+    }
+
+    @NonNull
+    public AceTelematicsTripDetailsModel getTripDetailsModel() {
+        return tripDetailsModel;
+    }
+
+}
